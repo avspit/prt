0b2b75a4efd22d922e5260f1f7fae9e5adb3edab
a18735215 <avshestakov@sberbank.ru>
2021-09-14 23:31:52 +0300

    add comments

--- Rosenblatt.py
+++ Rosenblatt.py
@@ -1,32 +1,65 @@
 import numpy as np
 
-'''
-Персептрон Розенблатта.
-Решение задач классификации для линейно-разделимых классов
-'''
 class Rosenblatt():
+    """
+    Персептрон Розенблатта.
+    Решение задач классификации для линейно-разделимых классов
+    """
 
     def __init__(self, alfa=0.1, epochs=10):
+        """
+        Инициализация
+
+        Параметры
+        ---------
+        alfa : Темп обучения
+        epochs : Количество проходов (эпох)
+        """
+
         self.alfa = alfa
         self.epochs = epochs
 
     def fit(self, X, T):
-        self.w = np.zeros(1 + X.shape[1])
+        """
+        Подгонка модели, используя тренировочные данные
+
+        Параметры
+        ---------
+        X : Тренировочные данные
+        T : Классы тренировочных данных
+        """
+
+        # Веса модели
+        self.weights = np.zeros(1 + X.shape[1])
+        # Количество ошибок в проходе
         self.errors = []
 
         for _ in range(self.epochs):
             errors = 0
             for coords, classes in zip(X, T):
                 update = self.alfa * (classes - self.predict(coords))
-                self.w[1:] += update * coords
-                self.w[0] += update
+                self.weights[1:] += update * coords
+                self.weights[0] += update
                 errors += int(update != 0.0)
-                print(_)
             self.errors.append(errors)
         return self
 
     def net_input(self, X):
-        return np.dot(X, self.w[1:]) + self.w[0]
+        """
+            Расчет коэффициента F(w*x)
+
+            Параметры
+            ---------
+            X : Тренировочные данные
+        """
+        return np.dot(X, self.weights[1:]) + self.weights[0]
 
     def predict(self, X):
+        """
+            Расчет e
+
+            Параметры
+            ---------
+            X : Тренировочные данные
+        """
         return np.where(self.net_input(X) >= 0.0, 1, -1)
